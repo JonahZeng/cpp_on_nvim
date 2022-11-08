d6bf3f0e9266dcc9f2365ae9d16872a4ca7ce6bd
json.zeng <json.zeng@e-genesys.com>
2022-11-08 11:15:41 +0800

    fix lsp desc

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -84,7 +84,8 @@ cmp.setup.cmdline(':', {
   })
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local lspconfig = require'lspconfig'
 lspconfig.clangd.setup {
